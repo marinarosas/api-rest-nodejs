89ce433a49d1becb64908c704c372efcd0759dde
Marina Jaudy <marinajaudy@MacBook-Pro-de-Marina.local>
2024-04-28 22:51:38 -0300

    improvement: Added default databe client inside env index

--- src/database.ts
+++ src/database.ts
@@ -8,7 +8,7 @@ export const config: Knex.Config = {
       ? {
           filename: env.DATABASE_URL,
         }
-      : env.DATABASE_CLIENT,
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
